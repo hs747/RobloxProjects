754c93058b4c64bc74e987912c1ad94686c41698
Henry Shaw <henryshaw747@gmail.com>
2022-07-05 23:50:34 -0700

    empty main branch

--- src/client_scripts/Client.client.lua
+++
@@ -1,8 +0,0 @@
-local Framework = require(game.ReplicatedStorage.Source.Shared.Framework.Framework)
-
-local function main()
-	Framework:addContainer(game.ReplicatedStorage.Source.Client.Modules)
-	Framework:run()
-end
-
-main()
--- src/server_scripts/Server.server.lua
+++
@@ -1,8 +0,0 @@
-local Framework = require(game.ReplicatedStorage.Source.Shared.Framework.Framework)
-
-local function main()
-	Framework:addContainer(game.ServerStorage.Server.Modules)
-	Framework:run()
-end
-
-main()

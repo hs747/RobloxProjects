49d9a12228ad4fe31165ff1b22c49a83b7371be4
hs747 <46875522+hs747@users.noreply.github.com>
2022-07-09 19:48:35 -0700

    add inventory type defs

---
+++ src/shared/Data/Types/Inventory.lua
@@ -0,0 +1,14 @@
+export type Item= {
+	id: string,
+	item: number,
+	width: number,
+	height: number,
+}
+
+export type Container = {
+	id: string,
+	width: number,
+	height: number,
+}
+
+return {}

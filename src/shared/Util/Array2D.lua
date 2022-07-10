4c6b96d8c0997f18c8476bdd0b4b9ba6950485bd
hs747 <46875522+hs747@users.noreply.github.com>
2022-07-09 20:12:23 -0700

    add array2d

---
+++ src/shared/Util/Array2D.lua
@@ -0,0 +1,44 @@
+-- wafflechad
+-- memory continuous 2d array
+
+export type Array2D<t> = {t}
+
+local Array2D = {}
+
+local Array2DContext = {}
+Array2DContext.__index = Array2DContext
+
+function Array2DContext.new(sizeX, sizeY)
+    local self = setmetatable({}, Array2DContext)
+    self.sizeX = sizeX
+    self.sizeY = sizeY
+    return self
+end
+
+Array2D.newContext = Array2DContext.new
+
+function Array2DContext:create<T>(set: T): Array2D<T>
+    return Array2D.create(self.sizeX, self.sizeY, set)
+end
+
+function Array2DContext:set<T>(array: Array2D<T>, x, y, set: T)
+    return Array2D.set(array, self.sizeX, self.sizeY, x, y, set)
+end
+
+function Array2DContext:get<T>(array: Array2D<T>, x, y): T
+    return Array2D.get(array, self.sizeX, self.sizeY, x, y)
+end
+
+function Array2D.create<T>(sizeX, sizeY, set): Array2D<T>
+    return table.create(sizeX * sizeY , set)
+end
+
+function Array2D.set<T>(array: Array2D<T>, sizeX, sizeY, x, y, set: T)
+    array[sizeY * (y - 1) + x] = set
+end
+
+function Array2D.get<T>(array: Array2D<T>, sizeX, sizeY, x, y)
+    return array[sizeY * (y - 1) + x]
+end
+
+return Array2D

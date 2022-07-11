5e627b74e482d575d02952c2af79d02765f80c5e
hs747 <46875522+hs747@users.noreply.github.com>
2022-07-11 16:46:41 -0700

    add hydration & starvation mechanics

--- src/server/Modules/CharacterManager.lua
+++ src/server/Modules/CharacterManager.lua
@@ -7,7 +7,9 @@ local RunService = game:GetService("RunService")
 local MAX_HUNGER = 100
 local MAX_THIRST = 100
 local THIRST_DECLINE_BASE = 1 -- unit/second
-local HUNGER_DECLINE_BASE = 1 -- unit/second
+local HUNGER_DECLINE_BASE = 1
+local HEALTH_STARVING_DECLINE = 2 
+local HEALTH_DEHYDRATED_DECLINE = 2
 
 local STAT_UPDATE_RATE = 0.5 -- interval between stat updates
 local lastStatUpdate
@@ -38,6 +40,21 @@ function characterManager:modifyStat(character, statName, modifyCallback)
 	return modifyCallback(stat)
 end
 
+function characterManager:getHealth(character)
+	local humanoid = character:FindFirstChild("Humanoid")
+	return humanoid.Health
+end
+
+function characterManager:setHealth(character, health)
+	local humanoid = character:FindFirstChild("Humanoid")
+	humanoid.health = health
+end
+
+function characterManager:modifyHealth(character, modifyCallback)
+	local humanoid = character:FindFirstChild("Humanoid")
+	humanoid.Health = modifyCallback(humanoid.Health)
+end
+
 function characterManager:start()
 	lastStatUpdate = os.clock()
 	RunService.Heartbeat:Connect(function() 
@@ -48,12 +65,24 @@ function characterManager:start()
 		lastStatUpdate = os.clock()
 		-- update stats
 		for _, character in ipairs(characters) do
-			self:modifyStat(character, "Thirst", function(thirst)
-				thirst.Value -= THIRST_DECLINE_BASE * timeDelta
+			local isDehydrated = self:modifyStat(character, "Thirst", function(thirst)
+				thirst.Value = math.max(thirst.Value - THIRST_DECLINE_BASE * timeDelta, 0)
+				return thirst.Value <= 0
 			end)
-			self:modifyStat(character, "Hunger", function(hunger)
-				hunger.Value -= HUNGER_DECLINE_BASE * timeDelta
+			local isStarving = self:modifyStat(character, "Hunger", function(hunger)
+				hunger.Value = math.max(hunger.Value - HUNGER_DECLINE_BASE * timeDelta, 0)
+				return hunger.Value <= 0
 			end)
+			if isDehydrated then
+				self:modifyHealth(character, function(health)
+					return health - HEALTH_DEHYDRATED_DECLINE
+				end)
+			end
+			if isStarving then
+				self:modifyHealth(character, function(health)
+					return health - HEALTH_STARVING_DECLINE
+				end)
+			end
 		end
 	end)
 end
